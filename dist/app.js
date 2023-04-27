ca4128926950f390a106545b450d5a6f5a7703a9
sn0rii <113938479+sn0rii@users.noreply.github.com>
2023-04-27 17:48:31 +0200

    update

--- dist/app.js
+++ dist/app.js
@@ -1,9 +1,38 @@
-const buttonElement = document.querySelector("button");
-const calculatePrice = (orginalPrice, hasDiscount) => {
-    return hasDiscount ? orginalPrice * 0.8 : orginalPrice;
+// <li>
+// <label for="task-1">Wyrzucić smieci</label>
+// <input type="checkbox" id="task-1" name="Wyrzucić smieci" />
+// </li>
+const taskNameInputElement = document.querySelector("#name");
+const addButtonElement = document.querySelector("button");
+const taskContainerElement = document.querySelector(".tasks");
+const tasks = [
+    {
+        name: "Wyrzuceniue smieci",
+        done: false,
+    },
+    {
+        name: "Pojsc na silownie",
+        done: true,
+    },
+    {
+        name: "Nakramic koty",
+        done: false,
+    },
+];
+const render = () => {
+    taskContainerElement.innerHTML = "";
+    tasks.forEach((task) => {
+        const taskElement = document.createElement("li");
+        taskElement.innerText = task.name;
+        taskContainerElement.appendChild(taskElement);
+    });
 };
-buttonElement.addEventListener("click", () => {
-    const orginalPrice = 50;
-    const hasDiscount = new URLSearchParams(window.location.search).get("discount") === "true";
-    console.log(calculatePrice(orginalPrice, hasDiscount));
+const addTask = (taskName) => {
+    tasks.push({ name: taskName, done: false });
+};
+addButtonElement.addEventListener("click", (event) => {
+    event.preventDefault();
+    addTask(taskNameInputElement.value);
+    render();
 });
+render();
--- src/app.ts
+++ src/app.ts
@@ -1,12 +1,46 @@
-const buttonElement = document.querySelector("button");
+// <li>
+// <label for="task-1">Wyrzucić smieci</label>
+// <input type="checkbox" id="task-1" name="Wyrzucić smieci" />
+// </li>
+const taskNameInputElement: HTMLInputElement = document.querySelector("#name");
+const addButtonElement: HTMLButtonElement = document.querySelector("button");
+const taskContainerElement: HTMLElement = document.querySelector(".tasks");
 
-const calculatePrice = (orginalPrice: number, hasDiscount: boolean) => {
-  return hasDiscount ? orginalPrice * 0.8 : orginalPrice;
+const tasks: {
+  name: string;
+  done: boolean;
+}[] = [
+  {
+    name: "Wyrzuceniue smieci",
+    done: false,
+  },
+  {
+    name: "Pojsc na silownie",
+    done: true,
+  },
+  {
+    name: "Nakramic koty",
+    done: false,
+  },
+];
+
+const render = () => {
+  taskContainerElement.innerHTML = "";
+  tasks.forEach((task) => {
+    const taskElement: HTMLElement = document.createElement("li");
+    taskElement.innerText = task.name;
+    taskContainerElement.appendChild(taskElement);
+  });
 };
 
-buttonElement.addEventListener("click", () => {
-  const orginalPrice: number = 50;
-  const hasDiscount: boolean =
-    new URLSearchParams(window.location.search).get("discount") === "true";
-  console.log(calculatePrice(orginalPrice, hasDiscount));
+const addTask = (taskName: string) => {
+  tasks.push({ name: taskName, done: false });
+};
+
+addButtonElement.addEventListener("click", (event: Event) => {
+  event.preventDefault();
+  addTask(taskNameInputElement.value);
+  render();
 });
+
+render();
